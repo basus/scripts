2ce4e2c9da74c8e87f4a1ec9f4037a097ea800bc
Shrutarshi Basu <shr@basus.me>
2025-02-03 19:35:01 +0530

    Update wyd: Report unpushed commits and uncommitted changes

--- wyd.sh
+++ wyd.sh
@@ -3,6 +3,7 @@
 reset='\033[0m'       # Reset
 green='\033[0;32m'    # Green
 blue='\033[0;34m'     # Blue
+purple='\033[0;35m'   # Purple
 
 # If there is a directory argument given, go to that directory
 if [ ! -z $1 ] && [ -d $1 ]; then
@@ -26,20 +27,22 @@ do
             msgs=$(git log --since=midnight --pretty=format:%w\(0,4,4\)%s 2>&1)
 
             # Also get the git status for uncommitted changes
-            gs=$(git status -s)
+            changed=$(git status -s)
+            ahead=$(git log --branches --not --remotes --oneline)
 
             # If the msgs are non-empty and don't start with "fatal", print them
             # The "fatal" check is required for git repos that have no commits yet
             # Print +more at the end if there are uncommitted changes
             if [ ! -z "$msgs" ] && [ "$msgs" = "${msgs#fatal}" ]; then
-                [ ! -z "$gs" ] && u="+more\n" || u=""
-                echo "In $green$dir$reset:"
+                [ ! -z "$changed" ] && c=" (${blue}changed$reset)" || c=""
+                [ ! -z "$ahead" ] && a=" (${purple}ahead$reset)" || a=""
+                echo "In $green$dir$reset:$c$a"
                 echo "$msgs"
-                echo "    ${blue}$u${reset}"
+                echo ""
             fi;
 
             # Check if there are uncommitted changes, save the dir if there are
-            if [ ! -z "$gs" ]; then
+            if [ ! -z "$changed" ]; then
                 set -- "$@" $dir
             fi;
         fi;
